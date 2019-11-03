1d87d0945be512c4e6e11e8d705837e130164174
Alex Luong <alex@alexluong.com>
2019-11-03 01:03:20 -0500

    feat(modal): initial modal impl with reach/dialog and react-spring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { animated } from "react-spring";
 import Sidebar, { useSidebar } from "../components/Sidebar";
+import Modal from "../components/Modal";
 
 function IndexPage() {
+  // Sidebar
   const {
     isMobile,
     sidebarWidth,
@@ -16,8 +18,12 @@ function IndexPage() {
   useSidebarLayout();
   const sidebarStyle = useSidebarStyle();
   const mainStyle = useMainStyle();
-
   const bindMain = useDragMain();
+
+  // Modal
+  const [showDialog, setShowDialog] = React.useState(false);
+  const close = () => setShowDialog(false);
+  const toggleModal = () => setShowDialog(!showDialog);
 
   return (
     <div className="min-h-screen relative overflow-x-hidden">
@@ -34,13 +40,19 @@ function IndexPage() {
         style={mainStyle}
       >
         <p className="text-teal-600">Hello, World!</p>
+        <button className="btn btn-primary ml-0 m-4" onClick={toggleModal}>
+          Toggle modal
+        </button>
+
         <button
           className="btn btn-primary"
           style={{ marginBottom: 1500 }}
           onClick={() => toggleSidebar()}
         >
-          Open/Close
+          Toggle sidebar
         </button>
+
+        <Modal showDialog={showDialog} close={close} />
 
         <p>Hello</p>
       </animated.div>
